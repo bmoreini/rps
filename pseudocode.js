f068883d92fe8a3f91df8449634a517924826f7d
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-04-18 13:35:56 -0400

    Update and rename rps.js to pseudocode.js

---
+++ pseudocode.js
@@ -0,0 +1,33 @@
+/* RPS Fairplay with If/Else by Ben W.*/
+
+/* Initialize Variables */
+
+var cpu = 0, choice = 0, cpuScore = 0, myScore = 0;
+
+/* Main */
+// get Player Turn
+// check if quit and stop if quit
+// make CPU Turn
+// check if same, redo if so
+// determine Winner
+// add score to winner
+// declare winner and score
+
+/* CPU Turn */
+// Make random number
+// Translate to R, P, S
+// return value as cpuTurn
+
+/* Player Turn */
+// Ask for player turn and store value
+// return value as playerTurn
+
+/* Winner Finder */
+// get playerTurn and cpuTurn as parameters
+// determine winner as player or cpu
+// return winner
+
+/* Wordify */
+// get R, P, S, Q
+// Look up word beginning with that letter
+// return word
