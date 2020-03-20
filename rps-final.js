0991028765d1c52897f43dbd04bdbb17d42e5d51
bmoreini <42300498+bmoreini@users.noreply.github.com>
2020-03-20 09:06:33 -0400

    Update rps-final.js

--- rps-final.js
+++ rps-final.js
@@ -5,66 +5,62 @@ var winner = -1;
 main ();
 
 function main () {
-	var outOf = parseInt (prompt ("Best out of?"));
+	var outOf = parseInt(prompt("Best out of?"));
 	for (var turn = 0; turn < outOf; turn ++) {
-		var pChoice = userTurn ();
+		var pChoice = userTurn();
 		if (pChoice == "q") turn = outOf;
 		else {
-			var cChoice = cpuTurn ();
+			var cChoice = cpuTurn();
 			if (pChoice == cChoice) {
-				alert ("We both chose" + letter2Number (cChoice));
+				alert("We both chose" + letter2Number(cChoice));
 				turn--;
 			}
 			else {
-				var winner = turnWinner (pChoice, cChoice);
-				update score (winner);
+				var winner = turnWinner(pChoice, cChoice);
+				updateScore(winner);
 			}
 		}
 	}
 	gameWinner();
 }
 
-function userTurn () {
-	var pChoice = parseInt (prompt ("choose either r (rock), p (paper) or s (scissors):"));
-	turnNumber = letter2Number (pChoice);
-	if (turn number == - 1) {
-		alert ("bad guess. Choose either r (Rock), p (Paper), s (scissors):");
-		userTurn ();
-		
+function userTurn() {
+	var pChoice = prompt ("choose either r (rock), p (paper) or s (scissors):");
+	turnNumber = letter2Number(pChoice);
+	if (turNumber == - 1) {
+		alert("Bad guess. Choose either r (Rock), p (Paper), s (scissors):");
+		userTurn();
 	}
 	else return pChoice;
 }
 
-function letter2Number (turnLetter) {
+function letter2Number(turnLetter) {
 	var turnNumber = -1;
 	for (i = 0; i <rpsWords.length; i ++) {
-		if (turnLetter == rpsWords [i] [0]) turnNumber = i;
+		if (turnLetter == rpsWords[i][0]) turnNumber = i;
 	}
 	return turnNumber;
 }
 
 function cpuTurn () {
 	var cChoice = Math.floor (Math.random () * 3);
-	alert ("I choose" + rpsWords [cChoice] [0] + ".")
+	alert("I choose" + rpsWords[cChoice] [0] + ".")
 	return cChoice;
 }
 
-function turnWinner (userTurn, cpuTurn) {
-	for (var index = 0; index <setWinner.length; index ++) {
-		if (userTurn == setWinner [index] [0] && cpuTurn == setWinner [index] [1]) {
-			winner = setWinner [index] [2];
-			alert ("You picked" + rpsWords [userTurn] + "and I picked" + rpsWords [cpuTurn] + ".");
-			alert (setWinner [index] [3] + "won!");
-			
+function turnWinner(userTurn, cpuTurn) {
+	for (var index = 0; index < setWinner.length; index ++) {
+		if (userTurn == setWinner[index] [0] && cpuTurn == setWinner[index] [1]) {
+			winner = setWinner[index] [2];
+			alert ("You picked" + rpsWords[userTurn] + "and I picked" + rpsWords[cpuTurn] + ".");
+			alert (setWinner[index][3] + "won!");
 		}
 	}
 	return winner;
 }
 
 function updateScore (winner) {
-	// score keeper [winner] ++;
-	score keeper [winner] = scorekeeper [winner] +1;
-	alert (score keeper);
+	scoreKeeper[winner] = scoreKeeper[winner] +1;
 }
 
 function gameWinner() {
